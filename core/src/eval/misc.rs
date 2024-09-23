7810a95fd5724c728408d2e1d414d494155ade6a
Lexterl33t <paraghostsociety@gmail.com>
2024-09-23 22:00:04 +0200

    fix order execution tracing

--- core/src/eval/misc.rs
+++ core/src/eval/misc.rs
@@ -8,8 +8,6 @@ use primitive_types::{H256, U256};
 
 #[inline]
 pub fn codesize(state: &mut Machine) -> Control {
-	let size = U256::from(state.code.len());
-	push_u256!(state, size);
 	#[cfg(feature = "tracing")]
 	{
 		use crate::Opcode;
@@ -20,11 +18,24 @@ pub fn codesize(state: &mut Machine) -> Control {
 			memory: state.memory(),
 		});
 	}
+	let size = U256::from(state.code.len());
+	push_u256!(state, size);
+	
 	Control::Continue(1)
 }
 
 #[inline]
 pub fn codecopy(state: &mut Machine) -> Control {
+	#[cfg(feature = "tracing")]
+	{
+		use crate::Opcode;
+		event!(DebuggingWithoutOperand{
+			opcode: Opcode(state.code[state.pc]),
+			position: &Ok(state.pc),
+			stack: state.stack(),
+			memory: state.memory(),
+		});
+	}
 	pop_u256!(state, memory_offset, code_offset, len);
 
 	// If `len` is zero then nothing happens, regardless of the
@@ -37,16 +48,7 @@ pub fn codecopy(state: &mut Machine) -> Control {
 	let memory_offset = as_usize_or_fail!(memory_offset);
 
 	try_or_fail!(state.memory.resize_offset(memory_offset, len));
-	#[cfg(feature = "tracing")]
-	{
-		use crate::Opcode;
-		event!(DebuggingWithoutOperand{
-			opcode: Opcode(state.code[state.pc]),
-			position: &Ok(state.pc),
-			stack: state.stack(),
-			memory: state.memory(),
-		});
-	}
+	
 	match state
 		.memory
 		.copy_large(memory_offset, code_offset, len, &state.code)
@@ -58,6 +60,16 @@ pub fn codecopy(state: &mut Machine) -> Control {
 
 #[inline]
 pub fn calldataload(state: &mut Machine) -> Control {
+	#[cfg(feature = "tracing")]
+	{
+		use crate::Opcode;
+		event!(DebuggingWithoutOperand{
+			opcode: Opcode(state.code[state.pc]),
+			position: &Ok(state.pc),
+			stack: state.stack(),
+			memory: state.memory(),
+		});
+	}
 	pop_u256!(state, index);
 
 	let mut load = [0u8; 32];
@@ -74,23 +86,12 @@ pub fn calldataload(state: &mut Machine) -> Control {
 	}
 
 	push_h256!(state, H256::from(load));
-	#[cfg(feature = "tracing")]
-	{
-		use crate::Opcode;
-		event!(DebuggingWithoutOperand{
-			opcode: Opcode(state.code[state.pc]),
-			position: &Ok(state.pc),
-			stack: state.stack(),
-			memory: state.memory(),
-		});
-	}
+	
 	Control::Continue(1)
 }
 
 #[inline]
 pub fn calldatasize(state: &mut Machine) -> Control {
-	let len = U256::from(state.data.len());
-	push_u256!(state, len);
 	#[cfg(feature = "tracing")]
 	{
 		use crate::Opcode;
@@ -101,11 +102,24 @@ pub fn calldatasize(state: &mut Machine) -> Control {
 			memory: state.memory(),
 		});
 	}
+	let len = U256::from(state.data.len());
+	push_u256!(state, len);
+	
 	Control::Continue(1)
 }
 
 #[inline]
 pub fn calldatacopy(state: &mut Machine) -> Control {
+	#[cfg(feature = "tracing")]
+	{
+		use crate::Opcode;
+		event!(DebuggingWithoutOperand{
+			opcode: Opcode(state.code[state.pc]),
+			position: &Ok(state.pc),
+			stack: state.stack(),
+			memory: state.memory(),
+		});
+	}
 	pop_u256!(state, memory_offset, data_offset, len);
 
 	// See comment on `codecopy` about the `len == 0` case.
@@ -116,16 +130,7 @@ pub fn calldatacopy(state: &mut Machine) -> Control {
 	let memory_offset = as_usize_or_fail!(memory_offset);
 
 	try_or_fail!(state.memory.resize_offset(memory_offset, len));
-	#[cfg(feature = "tracing")]
-	{
-		use crate::Opcode;
-		event!(DebuggingWithoutOperand{
-			opcode: Opcode(state.code[state.pc]),
-			position: &Ok(state.pc),
-			stack: state.stack(),
-			memory: state.memory(),
-		});
-	}
+	
 	match state
 		.memory
 		.copy_large(memory_offset, data_offset, len, &state.data)
@@ -137,7 +142,6 @@ pub fn calldatacopy(state: &mut Machine) -> Control {
 
 #[inline]
 pub fn pop(state: &mut Machine) -> Control {
-	pop_u256!(state, _val);
 	#[cfg(feature = "tracing")]
 	{
 		use crate::Opcode;
@@ -148,16 +152,13 @@ pub fn pop(state: &mut Machine) -> Control {
 			memory: state.memory(),
 		});
 	}
+	pop_u256!(state, _val);
+	
 	Control::Continue(1)
 }
 
 #[inline]
 pub fn mload(state: &mut Machine) -> Control {
-	pop_u256!(state, index);
-	let index = as_usize_or_fail!(index);
-	try_or_fail!(state.memory.resize_offset(index, 32));
-	let value = state.memory.get_h256(index);
-	push_h256!(state, value);
 	#[cfg(feature = "tracing")]
 	{
 		use crate::Opcode;
@@ -168,6 +169,12 @@ pub fn mload(state: &mut Machine) -> Control {
 			memory: state.memory(),
 		});
 	}
+	pop_u256!(state, index);
+	let index = as_usize_or_fail!(index);
+	try_or_fail!(state.memory.resize_offset(index, 32));
+	let value = state.memory.get_h256(index);
+	push_h256!(state, value);
+	
 	Control::Continue(1)
 }
 
@@ -195,10 +202,6 @@ pub fn mstore(state: &mut Machine) -> Control {
 
 #[inline]
 pub fn mstore8(state: &mut Machine) -> Control {
-	pop_u256!(state, index, value);
-	let index = as_usize_or_fail!(index);
-	try_or_fail!(state.memory.resize_offset(index, 1));
-	let value = u8::try_from(value.low_u32() & 0xff).unwrap_or(u8::MAX);
 	#[cfg(feature = "tracing")]
 	{
 		use crate::Opcode;
@@ -209,6 +212,10 @@ pub fn mstore8(state: &mut Machine) -> Control {
 			memory: state.memory(),
 		});
 	}
+	pop_u256!(state, index, value);
+	let index = as_usize_or_fail!(index);
+	try_or_fail!(state.memory.resize_offset(index, 1));
+	let value = u8::try_from(value.low_u32() & 0xff).unwrap_or(u8::MAX);
 	match state.memory.set(index, &[value], Some(1)) {
 		Ok(()) => Control::Continue(1),
 		Err(e) => Control::Exit(e.into()),
@@ -217,8 +224,6 @@ pub fn mstore8(state: &mut Machine) -> Control {
 
 #[inline]
 pub fn jump(state: &mut Machine) -> Control {
-	pop_u256!(state, dest);
-	let dest = as_usize_or_fail!(dest, ExitError::InvalidJump);
 	#[cfg(feature = "tracing")]
 	{
 		use crate::Opcode;
@@ -229,6 +234,9 @@ pub fn jump(state: &mut Machine) -> Control {
 			memory: state.memory(),
 		});
 	}
+	pop_u256!(state, dest);
+	let dest = as_usize_or_fail!(dest, ExitError::InvalidJump);
+	
 
 	if state.valids.is_valid(dest) {
 		Control::Jump(dest)
@@ -239,7 +247,6 @@ pub fn jump(state: &mut Machine) -> Control {
 
 #[inline]
 pub fn jumpi(state: &mut Machine) -> Control {
-	pop_u256!(state, dest, value);
 	#[cfg(feature = "tracing")]
 	{
 		use crate::Opcode;
@@ -250,6 +257,8 @@ pub fn jumpi(state: &mut Machine) -> Control {
 			memory: state.memory(),
 		});
 	}
+	pop_u256!(state, dest, value);
+	
 
 	if value == U256::zero() {
 		Control::Continue(1)
@@ -265,7 +274,6 @@ pub fn jumpi(state: &mut Machine) -> Control {
 
 #[inline]
 pub fn pc(state: &mut Machine, position: usize) -> Control {
-	push_u256!(state, U256::from(position));
 	#[cfg(feature = "tracing")]
 	{
 		use crate::Opcode;
@@ -276,12 +284,13 @@ pub fn pc(state: &mut Machine, position: usize) -> Control {
 			memory: state.memory(),
 		});
 	}
+	push_u256!(state, U256::from(position));
+	
 	Control::Continue(1)
 }
 
 #[inline]
 pub fn msize(state: &mut Machine) -> Control {
-	push_u256!(state, state.memory.effective_len().into());
 	#[cfg(feature = "tracing")]
 	{
 		use crate::Opcode;
@@ -292,18 +301,12 @@ pub fn msize(state: &mut Machine) -> Control {
 			memory: state.memory(),
 		});
 	}
+	push_u256!(state, state.memory.effective_len().into());
 	Control::Continue(1)
 }
 
 #[inline]
 pub fn push(state: &mut Machine, n: usize, position: usize) -> Control {
-	let end = min(position + 1 + n, state.code.len());
-	let slice = &state.code[(position + 1)..end];
-	let mut val = [0u8; 32];
-	val[(32 - n)..(32 - n + slice.len())].copy_from_slice(slice);
-	let val = U256::from_big_endian(&val);
-
-	push_u256!(state, val);
 	#[cfg(feature = "tracing")]
 	{
 		use crate::Opcode;
@@ -315,46 +318,19 @@ pub fn push(state: &mut Machine, n: usize, position: usize) -> Control {
 			memory: state.memory(),
 		});
 	}
+	let end = min(position + 1 + n, state.code.len());
+	let slice = &state.code[(position + 1)..end];
+	let mut val = [0u8; 32];
+	val[(32 - n)..(32 - n + slice.len())].copy_from_slice(slice);
+	let val = U256::from_big_endian(&val);
+
+	push_u256!(state, val);
+	
 	Control::Continue(1 + n)
 }
 
 #[inline]
 pub fn push0(state: &mut Machine) -> Control {
-	let val = U256::zero();
-
-	push_u256!(state, val);
-	Control::Continue(1)
-}
-
-#[inline]
-pub fn push1(state: &mut Machine, position: usize) -> Control {
-	let b0 = u64::from(*state.code.get(position + 1).unwrap_or(&0));
-	let val = U256::from(b0);
-
-	push_u256!(state, val);
-	#[cfg(feature = "tracing")]
-	{
-		use crate::Opcode;
-		let x = u8::from(*state.code.get(position + 1).unwrap_or(&0));
-		let slice = vec![x];
-		event!(DebuggingWithOperand{
-			opcode: Opcode(state.code[position]),
-			operands: &slice,
-			position: &Ok(position),
-			stack: state.stack(),
-			memory: state.memory(),
-		});
-	}
-	Control::Continue(2)
-}
-
-#[inline]
-pub fn push2(state: &mut Machine, position: usize) -> Control {
-	let b0 = u64::from(*state.code.get(position + 1).unwrap_or(&0));
-	let b1 = u64::from(*state.code.get(position + 2).unwrap_or(&0));
-	let val = U256::from((b0 << 8) | b1);
-
-	push_u256!(state, val);
 	#[cfg(feature = "tracing")]
 	{
 		use crate::Opcode;
@@ -369,16 +345,61 @@ pub fn push2(state: &mut Machine, position: usize) -> Control {
 			memory: state.memory(),
 		});
 	}
+	let val = U256::zero();
+
+	push_u256!(state, val);
+	Control::Continue(1)
+}
+
+#[inline]
+pub fn push1(state: &mut Machine, position: usize) -> Control {
+	#[cfg(feature = "tracing")]
+	{
+		use crate::Opcode;
+		let x = u8::from(*state.code.get(position + 1).unwrap_or(&0));
+		let slice = vec![x];
+		event!(DebuggingWithOperand{
+			opcode: Opcode(state.code[position]),
+			operands: &slice,
+			position: &Ok(position),
+			stack: state.stack(),
+			memory: state.memory(),
+		});
+	}
+	let b0 = u64::from(*state.code.get(position + 1).unwrap_or(&0));
+	let val = U256::from(b0);
+
+	push_u256!(state, val);
+	Control::Continue(2)
+}
+
+#[inline]
+pub fn push2(state: &mut Machine, position: usize) -> Control {
+	#[cfg(feature = "tracing")]
+	{
+		use crate::Opcode;
+		let x = u8::from(*state.code.get(position + 1).unwrap_or(&0));
+		let x2 = u8::from(*state.code.get(position + 2).unwrap_or(&0));
+		let slice = vec![x, x2];
+		event!(DebuggingWithOperand{
+			opcode: Opcode(state.code[position]),
+			operands: &slice,
+			position: &Ok(position),
+			stack: state.stack(),
+			memory: state.memory(),
+		});
+	}
+	let b0 = u64::from(*state.code.get(position + 1).unwrap_or(&0));
+	let b1 = u64::from(*state.code.get(position + 2).unwrap_or(&0));
+	let val = U256::from((b0 << 8) | b1);
+
+	push_u256!(state, val);
+	
 	Control::Continue(3)
 }
 
 #[inline]
 pub fn dup(state: &mut Machine, n: usize) -> Control {
-	let value = match state.stack.peek(n - 1) {
-		Ok(value) => value,
-		Err(e) => return Control::Exit(e.into()),
-	};
-	push_u256!(state, value);
 	#[cfg(feature = "tracing")]
 	{
 		use crate::Opcode;
@@ -389,11 +410,27 @@ pub fn dup(state: &mut Machine, n: usize) -> Control {
 			memory: state.memory(),
 		});
 	}
+	let value = match state.stack.peek(n - 1) {
+		Ok(value) => value,
+		Err(e) => return Control::Exit(e.into()),
+	};
+	push_u256!(state, value);
+	
 	Control::Continue(1)
 }
 
 #[inline]
 pub fn swap(state: &mut Machine, n: usize) -> Control {
+	#[cfg(feature = "tracing")]
+	{
+		use crate::Opcode;
+		event!(DebuggingWithoutOperand{
+			opcode: Opcode(state.code[state.pc]),
+			position: &Ok(state.pc),
+			stack: state.stack(),
+			memory: state.memory(),
+		});
+	}
 	let val1 = match state.stack.peek(0) {
 		Ok(value) => value,
 		Err(e) => return Control::Exit(e.into()),
@@ -410,6 +447,11 @@ pub fn swap(state: &mut Machine, n: usize) -> Control {
 		Ok(()) => (),
 		Err(e) => return Control::Exit(e.into()),
 	}
+	Control::Continue(1)
+}
+
+#[inline]
+pub fn ret(state: &mut Machine) -> Control {
 	#[cfg(feature = "tracing")]
 	{
 		use crate::Opcode;
@@ -420,11 +462,6 @@ pub fn swap(state: &mut Machine, n: usize) -> Control {
 			memory: state.memory(),
 		});
 	}
-	Control::Continue(1)
-}
-
-#[inline]
-pub fn ret(state: &mut Machine) -> Control {
 	pop_u256!(state, start, len);
 	if len > U256::zero() {
 		let start = as_usize_or_fail!(start);
@@ -432,16 +469,6 @@ pub fn ret(state: &mut Machine) -> Control {
 		try_or_fail!(state.memory.resize_offset(start, len));
 	}
 	state.return_range = start..(start + len);
-	#[cfg(feature = "tracing")]
-	{
-		use crate::Opcode;
-		event!(DebuggingWithoutOperand{
-			opcode: Opcode(state.code[state.pc]),
-			position: &Ok(state.pc),
-			stack: state.stack(),
-			memory: state.memory(),
-		});
-	}
 	Control::Exit(ExitSucceed::Returned.into())
 }
 
